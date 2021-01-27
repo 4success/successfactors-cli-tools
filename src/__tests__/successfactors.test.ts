3fde54b14f3c451579d303bfe712b3845689f592
Renan William Alves de Paula <renan@4success.com.br>
2021-01-27 19:06:22 -0300

    Ajustes para testes

---
+++ src/__tests__/successfactors.test.ts
@@ -0,0 +1,14 @@
+import SuccessFactors from "../lib/successFactors";
+
+jest.mock('../lib/successFactors');
+
+beforeAll(() => {
+
+});
+
+test('testCredentials', async () => {
+    const sf = new SuccessFactors();
+
+    await sf.login();
+    expect(await sf.checkLogin()).toBe(true);
+});
---
+++ src/lib/__mocks__/successFactors.ts
@@ -0,0 +1,19 @@
+export default class SuccessFactors {
+    async login(): Promise<boolean> {
+        console.log('Mock Successfactors: login was called');
+        return true;
+    }
+
+    logout() {
+        console.log('Mock Successfactors: logout was called');
+    }
+
+    async checkLogin(): Promise<boolean> {
+        console.log('Mock Successfactors: checkLogin was called');
+        return true;
+    }
+
+    async downloadProfileImagesToFolder(): Promise<void> {
+        console.log('Mock Successfactors: downloadProfileImagesToFolder was called');
+    }
+}
